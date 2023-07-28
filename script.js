8638fe2f4800528009424f4bb65742a171ea07c4
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-07-28 12:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+function validateForm() {
+      const nameInput = document.getElementById("name");
+      const portfolioInput = document.getElementById("portfolio");
+
+      if (nameInput.value.length < 3 || nameInput.value.length > 50) {
+        alert("Name must be between 3 and 50 characters.");
+        return false;
+      }
+
+      // You can add further validation for the portfolio URL if required.
+
+      return true;
+    }
